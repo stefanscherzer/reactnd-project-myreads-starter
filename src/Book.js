026674282b6cfc66ac325fbdacdc072c9e4f7832
Stefan Scherzer <stefan.scherzer@kununu.com>
2017-09-07 21:46:27 +0200

    feat: display default image if no cover is given

--- src/Book.js
+++ src/Book.js
@@ -12,7 +12,9 @@ class Book extends Component {
       <li key={book.id}>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+            {book.imageLinks ?
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+              : <div className="book-no-cover"/>}
             <div className="book-shelf-changer">
               <select value="none" readOnly>
                 <option value="info" disabled>Move to...</option>

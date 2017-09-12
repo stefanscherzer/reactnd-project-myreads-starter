c3b207c2116692c138b5832464faf637c394cb01
Stefan Scherzer <stefan.scherzer@kununu.com>
2017-09-12 09:24:29 +0200

    refactor: introduced debouncing for the input field
    To avoid that every update on the search input filed calls the eventhandler, I use debouncing from now on.

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Debounce } from 'react-throttle';
 import * as BooksAPI from './BooksAPI'
 import Book from './Book'
 
@@ -63,12 +64,14 @@ class SearchBooks extends Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input
-              type='text'
-              placeholder='Search by title or author'
-              value={query}
-              onChange={(event) => this.searchBooks(event.target.value)}
-            />
+            <Debounce time="400" handler="onChange">
+                <input
+                  type='text'
+                  value={this.query}
+                  onChange={(event) => this.searchBooks(event.target.value)}
+                  placeholder='Search by title or author'
+                />
+            </Debounce>
             {query && (
               <div
                 className='clear-search'
